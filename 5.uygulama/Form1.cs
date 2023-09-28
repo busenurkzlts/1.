28db480df00e5697178c981482c4eba88a725a8e
busenurkzlts <118312648+busenurkzlts@users.noreply.github.com>
2023-09-28 19:14:07 +0300

    Add files via upload

---
+++ 5.uygulama/Form1.cs
@@ -0,0 +1,18 @@
+using System.Windows.Forms;
+
+namespace _5.uygulama
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "hello";
+
+        }
+    }
+}
